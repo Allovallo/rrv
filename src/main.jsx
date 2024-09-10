87646e423052d7c042ce0e15a8b405d8cf069115
Allovallo <grossco@meta.ua>
2024-09-10 15:03:02 +0300

    files updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import Root, {loader as rootLoader, action as rootAction} from './routes/root'
 import ErrorPage from './error-page'
-import Contact, { loader as contactLoader, } from './routes/contact'
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from './routes/contact'
 import EditContact, { action as editAction, } from './routes/edit'
 import { action as destroyAction } from './routes/destroy'
 import Index from './routes'
@@ -18,6 +21,9 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
+        errorElement: <ErrorPage />,
+        children: [
+          {
         index: true,
         element: <Index/>,
       },
@@ -25,6 +31,7 @@ const router = createBrowserRouter([
         path: 'contacts/:contactId',
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction,
       },
       {
         path: 'contacts/:contactId/edit',
@@ -37,6 +44,10 @@ const router = createBrowserRouter([
         action: destroyAction,
         errorElement: <div>Oops! There was an error.</div>,
       },
+        ]
+      }
+    
+      
     ]
   },
   
